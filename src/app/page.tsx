55e2f133b92bc1f0adae4c8d5820348830cee9b6
NBN7 <nqchossj@gmail.com>
2024-03-13 19:11:14 -0300

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ import { IoArrowForward, IoLogoGithub } from "react-icons/io5";
 export default function HomePage() {
   return (
     <main>
-      <Container sectionClassName="mt-20 font-semibold">
-        <div className="flex flex-col gap-6 text-pretty">
+      <Container sectionClassName="mt-20">
+        <div className="flex flex-col gap-6">
           {/* title */}
-          <h1 className="text-4xl">
+          <h1 className="text-4xl font-semibold text-pretty">
             <span className="from-[#0070F0] to-[#19cfff] bg-clip-text text-transparent bg-gradient-to-b">
               Jot it down
             </span>
