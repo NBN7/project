b8edffdad01602c4b5b04e2e1cf5f683c1610e06
Ignacio Biran <nqchossj@gmail.com>
2024-03-13 05:09:10 -0300

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  // Si el usuario está autenticado y está accediendo a /auth, redirígelo.
+  if (token && pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!token && pathname === "/") {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  // Aplica el middleware solo a las rutas especificadas.
+  if (
+    pathname.startsWith("/home") ||
+    pathname.startsWith("/profile") ||
+    pathname.startsWith("/dashboard")
+  ) {
+    // Aquí puedes agregar cualquier otra lógica de middleware que necesites.
+    if (!token) {
+      return NextResponse.redirect(new URL("/auth", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/home:path*",
+    "/profile/:path*",
+    "/dashboard:path*",
+    "/auth:path*",
+  ],
+};
