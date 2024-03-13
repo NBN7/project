0d7488e36286662ad8471de4230c38e1fb7b5289
NBN7 <nqchossj@gmail.com>
2024-03-13 19:10:40 -0300

    update auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,8 +4,18 @@ import { Container } from "@/components/Container";
 export default function AuthPage() {
   return (
     <Container sectionClassName="mt-20">
-      <h1 className="text-2xl font-semibold">Sign In</h1>
-      <SignInButton className="w-[200px]" />
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl font-semibold">
+          Welcome to{" "}
+          <span className="from-[#0070F0] to-[#19cfff] bg-clip-text text-transparent bg-gradient-to-b">
+            Notes
+          </span>
+        </h1>
+        <p className="dark:text-greydark text-greylight">
+          Sign In to make the most of the app
+        </p>
+        <SignInButton className="w-[200px] mt-4" />
+      </div>
     </Container>
   );
 }
