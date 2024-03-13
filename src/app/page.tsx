59c7f9f62e336f078acf336d23aa9186a581f628
Ignacio Biran <nqchossj@gmail.com>
2024-03-13 05:52:56 -0300

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Container } from "@/components/Container";
+
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <main>
+      <Container sectionClassName="mt-20 font-semibold">
+        <div className="text-4xl text-pretty">
+          <p>
+            <span className=" from-[#0070F0] to-[#19cfff] bg-clip-text text-transparent bg-gradient-to-b">
+              Jot it down
+            </span>
+            , make it last, transform your day.
+          </p>
+        </div>
+      </Container>
+    </main>
+  );
 }
