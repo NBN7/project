549af308bb12412423ca47256ea19d1301128f85
Ignacio Biran <nqchossj@gmail.com>
2024-03-30 16:49:35 -0300

    add shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,24 @@ import { nextui } from "@nextui-org/react";
 
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       textColor: {
         darkmode: "#ECEDEE",
@@ -24,8 +33,23 @@ const config: Config = {
         darkmode: "#141518",
         lightmode: "#FFFFFF",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [nextui()],
-};
+  plugins: [nextui(), require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
