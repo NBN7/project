457ffa85ce1914cbc7a27c7b42b84d303724eecd
Ignacio Biran <nqchossj@gmail.com>
2024-04-30 03:44:57 -0300

    update github link

--- src/components/CustomButtons/GithubButton.tsx
+++ src/components/CustomButtons/GithubButton.tsx
@@ -6,7 +6,7 @@ import { IoLogoGithub } from "react-icons/io5";
 
 export const GithubButton = () => {
   return (
-    <Link href="https://github.com/NBN7" target="_blank">
+    <Link href="https://github.com/NBN7/project" target="_blank">
       <Button variant="outline" className="sm:w-[200px] w-full">
         <IoLogoGithub className="mr-2 size-5" />
         <span>GitHub</span>
