010c224350586d48f4ec4179a0bcda3ae54aa1bc
Ignacio Biran <nqchossj@gmail.com>
2024-04-29 22:18:33 -0300

    fix ui bug

--- src/app/transactions/create/page.tsx
+++ src/app/transactions/create/page.tsx
@@ -63,101 +63,103 @@ export default function CreatePage() {
   };
 
   return (
-    <Card className="w-full duration-500 animate-in fade-in-5 slide-in-from-bottom-2">
-      <CardHeader>
-        <CardTitle>New transaction</CardTitle>
-        <CardDescription>
-          Create a new transaction by filling out the form below.
-        </CardDescription>
-      </CardHeader>
+    <div className="duration-500 animate-in fade-in-5 slide-in-from-bottom-2">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>New transaction</CardTitle>
+          <CardDescription>
+            Create a new transaction by filling out the form below.
+          </CardDescription>
+        </CardHeader>
 
-      <CardContent>
-        <div className="flex flex-col gap-2">
-          <Input
-            value={description}
-            maxLength={32}
-            className="sm:w-1/2 focus-visible:ring-offset-0 focus-visible:ring-0"
-            placeholder="Description"
-            name="description"
-            autoComplete="off"
-            onChange={(e) => setDescription(e.target.value)}
-          />
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <Input
+              value={description}
+              maxLength={32}
+              className="sm:w-1/2 focus-visible:ring-offset-0 focus-visible:ring-0"
+              placeholder="Description"
+              name="description"
+              autoComplete="off"
+              onChange={(e) => setDescription(e.target.value)}
+            />
 
-          <Select
-            onValueChange={(type) =>
-              setType(type.toLocaleLowerCase() as TransactionType)
+            <Select
+              onValueChange={(type) =>
+                setType(type.toLocaleLowerCase() as TransactionType)
+              }
+            >
+              <SelectTrigger className="w-[180px] focus:ring-transparent">
+                <SelectValue placeholder="Select type" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  {TRANSACTION_TYPES.map((transaction, index) => (
+                    <SelectItem
+                      className="capitalize"
+                      key={index}
+                      value={transaction}
+                    >
+                      {transaction}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <Input
+              value={!amount ? "" : amount}
+              className="sm:w-1/2 focus-visible:ring-offset-0 focus-visible:ring-0"
+              placeholder="Amount"
+              name="amount"
+              type="number"
+              onChange={(e) => setAmount(parseFloat(e.target.value))}
+            />
+
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="w-[180px] pl-3 text-left font-normal"
+                >
+                  <span>{date?.toLocaleDateString()}</span>
+                  <CalendarIcon className="ml-auto size-4 dark:text-icondark text-iconlight" />
+                </Button>
+              </PopoverTrigger>
+
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  disabled={(date) =>
+                    date > new Date() || date < new Date("1900-01-01")
+                  }
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <Button
+            disabled={
+              !description.length ||
+              !type ||
+              amount <= 0 ||
+              !amount ||
+              !date ||
+              isPending
             }
+            className="w-full"
+            onClick={handleCreateTransaction}
           >
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select type" />
-            </SelectTrigger>
-
-            <SelectContent>
-              <SelectGroup>
-                {TRANSACTION_TYPES.map((transaction, index) => (
-                  <SelectItem
-                    className="capitalize"
-                    key={index}
-                    value={transaction}
-                  >
-                    {transaction}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-
-          <Input
-            value={!amount ? "" : amount}
-            className="sm:w-1/2 focus-visible:ring-offset-0 focus-visible:ring-0"
-            placeholder="Amount"
-            name="amount"
-            type="number"
-            onChange={(e) => setAmount(parseFloat(e.target.value))}
-          />
-
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                className="w-[180px] pl-3 text-left font-normal"
-              >
-                <span>{date?.toLocaleDateString()}</span>
-                <CalendarIcon className="ml-auto size-4 dark:text-icondark text-iconlight" />
-              </Button>
-            </PopoverTrigger>
-
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                disabled={(date) =>
-                  date > new Date() || date < new Date("1900-01-01")
-                }
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
-      </CardContent>
-
-      <CardFooter>
-        <Button
-          disabled={
-            !description.length ||
-            !type ||
-            amount <= 0 ||
-            !amount ||
-            !date ||
-            isPending
-          }
-          className="w-full"
-          onClick={handleCreateTransaction}
-        >
-          Create transaction
-        </Button>
-      </CardFooter>
-    </Card>
+            Create transaction
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
