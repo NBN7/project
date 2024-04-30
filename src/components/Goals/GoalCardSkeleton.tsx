d79aeff12071fdb5504d160992743288596c7fd9
Ignacio Biran <nqchossj@gmail.com>
2024-04-30 17:48:42 -0300

    update goal card skeleton | goals empty

--- src/components/Goals/GoalCardSkeleton.tsx
+++ src/components/Goals/GoalCardSkeleton.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from "../ui/skeleton";
 
 export const GoalCardSkeleton = () => {
-  return <Skeleton className="w-full h-[116px] rounded" />;
+  return <Skeleton className="w-full h-[134px] rounded" />;
 };
