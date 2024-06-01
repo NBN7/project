99d6d28633ec11bf09d604eb8c04f1dac26bf72b
Ignacio Biran <nqchossj@gmail.com>
2024-06-01 06:42:58 -0300

    update

--- src/components/UsersTable/columns.tsx
+++ src/components/UsersTable/columns.tsx
@@ -2,12 +2,11 @@
 
 import { useState } from "react";
 
-import { ColumnDef } from "@tanstack/react-table";
-
-import { EditUserDialog } from "@/components/UsersTable/Dialog/EditUserDialog";
-import { DeleteUserDialog } from "@/components/UsersTable/Dialog/DeleteUserDialog";
+import { ColumnDef, Row } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
+import { EditUserDialog } from "@/components/UsersTable/Dialog/EditUserDialog";
+import { DeleteUserDialog } from "@/components/UsersTable/Dialog/DeleteUserDialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,11 +17,61 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
-import type { TUser } from "@/types/user";
-
 import { toastCall } from "@/utils/toastCall";
 
+import type { TUser } from "@/types/user";
+
 import { IoIosMore } from "react-icons/io";
+
+interface ActionCellProps {
+  row: Row<TUser>;
+}
+
+const ActionCell: React.FC<ActionCellProps> = ({ row }) => {
+  const user = row.original;
+  const [currentDialog, setCurrentDialog] = useState<string>("");
+
+  const handleCopyId = () => {
+    navigator.clipboard.writeText(user.id);
+    toastCall("ID copied to clipboard");
+  };
+
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0 rounded-full">
+            <span className="sr-only">Open menu</span>
+            <IoIosMore className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent className="flex flex-col" align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={handleCopyId}>Copy ID</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DialogTrigger onClick={() => setCurrentDialog("edit")}>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+          </DialogTrigger>
+          <DialogTrigger onClick={() => setCurrentDialog("delete")}>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DialogTrigger>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DialogContent
+        className="w-11/12 sm:max-w-md"
+        onOpenAutoFocus={(e) => e.preventDefault()}
+      >
+        {currentDialog === "delete" ? (
+          <DeleteUserDialog id={user.id} />
+        ) : currentDialog === "edit" ? (
+          <EditUserDialog user={user} />
+        ) : null}
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export const columns: ColumnDef<TUser>[] = [
   {
@@ -39,65 +88,6 @@ export const columns: ColumnDef<TUser>[] = [
   },
   {
     id: "actions",
-    cell: ({ row }) => {
-      const user = row.original;
-
-      const [currentDialog, setCurrentDialog] = useState("");
-
-      const handleCopyId = () => {
-        navigator.clipboard.writeText(user.id);
-
-        toastCall("ID copied to clipboard");
-      };
-
-      return (
-        <Dialog>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0 rounded-full">
-                <span className="sr-only">Open menu</span>
-                <IoIosMore className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent className="flex flex-col" align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem onClick={handleCopyId}>
-                Copy ID
-              </DropdownMenuItem>
-
-              <DropdownMenuSeparator />
-
-              <DialogTrigger
-                onClick={() => {
-                  setCurrentDialog("edit");
-                }}
-              >
-                <DropdownMenuItem>Edit</DropdownMenuItem>
-              </DialogTrigger>
-
-              <DialogTrigger
-                onClick={() => {
-                  setCurrentDialog("delete");
-                }}
-              >
-                <DropdownMenuItem>Delete</DropdownMenuItem>
-              </DialogTrigger>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          <DialogContent
-            className="w-11/12 sm:max-w-md"
-            onOpenAutoFocus={(e) => e.preventDefault()}
-          >
-            {currentDialog === "delete" ? (
-              <DeleteUserDialog id={user.id} />
-            ) : currentDialog === "edit" ? (
-              <EditUserDialog user={user} />
-            ) : null}
-          </DialogContent>
-        </Dialog>
-      );
-    },
+    cell: ActionCell,
   },
 ];
