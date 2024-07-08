641a85801ee0621598112efdf69e07613c36941e
Ignacio Biran <nqchossj@gmail.com>
2024-07-08 19:38:59 -0300

    update go back button

--- src/components/CustomButtons/GoBackButton.tsx
+++ src/components/CustomButtons/GoBackButton.tsx
@@ -1,21 +1,23 @@
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+
+import { ROUTES } from "@/constants";
 
 import { IoChevronBack } from "react-icons/io5";
 
 export const GoBackButton = () => {
-  const router = useRouter();
-
   const iconSize = "20px";
 
   return (
-    <Button
-      variant="ghost"
-      className="dark:text-icondark text-iconlight rounded-full"
-      size="icon"
-    >
-      <IoChevronBack size={iconSize} onClick={() => router.back()} />
-    </Button>
+    <Link href={ROUTES.HOME}>
+      <Button
+        variant="ghost"
+        className="dark:text-icondark text-iconlight rounded-full"
+        size="icon"
+      >
+        <IoChevronBack size={iconSize} />
+      </Button>
+    </Link>
   );
 };
