7559a50790544384ad17dd4669fc4aa96021a7e5
NBN7 <nqchossj@gmail.com>
2024-11-29 20:07:18 -0300

    update viewport height to dynamic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen dark:bg-darkmode dark:text-darkmode bg-lightmode text-lightmode">
+      <body className="min-h-[100dvh] dark:bg-darkmode dark:text-darkmode bg-lightmode text-lightmode">
         <Providers>
           <NavbarComponent />
           {children}
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ export default function LoadingPage() {
   const iconSize = "20px";
 
   return (
-    <main className="w-full h-[calc(100vh-64px)] flex items-center justify-center">
+    <main className="w-full h-[calc(100dvh-64px)] flex items-center justify-center">
       <Loader2 size={iconSize} className="animate-spin" />
     </main>
   );
